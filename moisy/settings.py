7d691441406e459f465b911719657e2a44de684e
ZhengHuahua <sven.zhenghh@gmail.com>
2015-11-10 21:54:12 +0800

    add static root

--- moisy/settings.py
+++ moisy/settings.py
@@ -96,5 +96,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-#STATIC_ROOT = 'D:/Startup/enterprisesoftware/django/test/'
+STATIC_ROOT = '/opt/bitnami/apps/django/django_projects/moisy/moisy/'
 STATIC_URL = '/moisy/static/'
