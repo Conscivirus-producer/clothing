de0c8706a5a2a3d84dda045c4556ba4f9616f6fb
ZhengHuahua <sven.zhenghh@gmail.com>
2015-11-11 12:31:36 +0800

    change static folder setting

--- moisy/settings.py
+++ moisy/settings.py
@@ -96,5 +96,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = '/opt/bitnami/apps/django/django_projects/moisy/moisy/static/'
+STATIC_ROOT = '/opt/bitnami/apps/django/django_projects/moisy/static/'
 STATIC_URL = '/static/'
